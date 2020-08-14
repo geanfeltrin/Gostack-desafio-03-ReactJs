88f84c6d0638205431b7f6a255fe239c272ffb8f
Gean Feltrin <gean_costa@hotmail.com>
2020-08-14 15:52:42 -0300

    Create function import

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+};
+
+export const formatDate = (date: Date): string => {
+  return Intl.DateTimeFormat('pt-BR').format(new Date(date));
+};
 
 export default formatValue;
